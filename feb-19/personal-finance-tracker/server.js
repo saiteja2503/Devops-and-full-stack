2a5da30dc66d2f70d6fc7f024aa393fbedc20a1d
saiteja <2303a52325@sru.edu.in>
2026-02-19 10:41:14 +0530

    Added Dockerized personal finance tracker

---
+++ feb-19/personal-finance-tracker/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Personal Finance Tracker API');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
